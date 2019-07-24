c1abd5d715ac540cb27143d740bfaa88f51df940
MicahYin <yinwq887@163.com>
2019-07-24 16:31:00 +0800

    Init

---
+++ src/main/java/com/guet/controller/Test.java
@@ -0,0 +1,13 @@
+package com.guet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author MicahYin
+ * @date 2019/7/24 16:20
+ */
+@Controller
+@RequestMapping("/test")
+public class Test {
+}
---
+++ src/main/java/com/guet/dao/TestDao.java
@@ -0,0 +1,11 @@
+package com.guet.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author MicahYin
+ * @date 2019/7/24 16:21
+ */
+@Repository
+public class TestDao {
+}
